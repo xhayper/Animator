b22c2ecdcbca3cbebc92c6ae99a60e006b0ab05f
xhayper <46065349+xhayper@users.noreply.github.com>
2021-07-01 19:16:02 +0700

    Stuff

--- Source/Animator.lua
+++ Source/Animator.lua
@@ -7,6 +7,8 @@ local Utility = animatorRequire("Utility.lua")
 local Animator = {isPlaying = false, isStopped = false, Loop = false}
 Animator.__index = Animator
 
+local format = string.format
+
 function Animator.new(plr, Animation)
 	local c = setmetatable({}, Animator)
 
@@ -32,67 +34,61 @@ function Animator:Start()
 		self.isPlaying = true
 		self.isStopped = false
 		local chr = self.Player.Character
-		if chr then
-			spawn(function()
-				local originalHipHeight
-				if chr:FindFirstChild("Humanoid") then
-					if chr.Humanoid:FindFirstChild("Animator") and chr.Humanoid.Animator:IsA("Animator") then
-						chr.Humanoid.Animator:Destroy()
+		if not chr then return end
+		spawn(function()
+			local originalHipHeight
+			if chr:FindFirstChild("Humanoid") then
+				if chr.Humanoid:FindFirstChild("Animator") and chr.Humanoid.Animator:IsA("Animator") then
+					chr.Humanoid.Animator:Destroy()
+				end
+				originalHipHeight = chr.Humanoid.HipHeight
+				chr.Humanoid.HipHeight = self.AnimationData.AuthoredHipHeight
+			end
+			if chr:FindFirstChild("Animate") and chr.Animate:IsA("LocalScript") then
+				chr.Animate.Disabled = true
+			end
+			local RigMotor = Utility:getRigData(self.Player)
+			local lastTick = tick()
+			local lastFrameTime = 0
+			for _,Frame in pairs(self.AnimationData.Frames) do
+				if Frame.Time ~= 0 then
+					if tick() - lastTick < Frame.Time then
+						repeat RunService.Heartbeat:Wait() until tick() - lastTick >= Frame.Time
 					end
-					originalHipHeight = chr.Humanoid.HipHeight
-					chr.Humanoid.HipHeight = self.AnimationData.AuthoredHipHeight
 				end
-				if chr:FindFirstChild("Animate") and chr.Animate:IsA("LocalScript") then
-					chr.Animate.Disabled = true
-				end
-				local RigMotor = Utility:getRigData(self.Player)
-				local lastTick = tick()
-				local lastFrameTime = 0
-				for frameNumber,Frame in pairs(self.AnimationData.Frames) do
-					if Frame.Time ~= 0 then
-						if tick() - lastTick < Frame.Time then
-							repeat RunService.Heartbeat:Wait() until tick() - lastTick >= Frame.Time
+				if self.isStopped == true then break end
+				for PartName,Pose in pairs(Frame.Poses) do
+					local Tweeninfo = TweenInfo.new(Frame.Time - lastFrameTime, Pose.EasingStyle, Pose.EasingDirection)
+					if PartName == "HumanoidRootPart" then
+						chr.HumanoidRootPart.CFrame *= Pose.CFrame
+					else
+						local Motor = RigMotor[PartName]
+						if Motor then
+							TweenService:Create(Motor, Tweeninfo, {
+								Transform = Pose.CFrame
+							}):Play()
 						end
 					end
-					if self.isStopped == true then
-						break;
-					end
-					for PartName,Pose in pairs(Frame.Poses) do
-						local Tweeninfo = TweenInfo.new(Frame.Time - lastFrameTime, Pose.EasingStyle, Pose.EasingDirection)
-						if PartName == "HumanoidRootPart" then
-							chr.HumanoidRootPart.CFrame *= Pose.CFrame
-						else
-							local Motor = RigMotor[PartName]
-							if Motor then
-								TweenService:Create(Motor, Tweeninfo, {
-									Transform = Pose.CFrame
-								}):Play()
-							end
-						end
-					end
-					lastFrameTime = Frame.Time
 				end
-				self.isPlaying = false
-				if self.Loop == true and self.isStopped ~= true then
-					return self:Start()
-				else
-					wait()
-					local defaultCF = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-					for _,Motor in pairs(RigMotor) do
-						Motor.Transform = defaultCF
-					end
-					if chr:FindFirstChild("Humanoid") then
-						if originalHipHeight ~= nil then
-							chr.Humanoid.HipHeight = originalHipHeight
-						end
-						Instance.new("Animator", chr.Humanoid)
-					end
-					if chr:FindFirstChild("Animate") then
-						chr.Animate.Disabled = false
-					end
+				lastFrameTime = Frame.Time
+			end
+			self.isPlaying = false
+			if self.Loop == true and self.isStopped ~= true then return self:Start() end
+			wait()
+			local defaultCF = CFrame.new(0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+			for _,Motor in pairs(RigMotor) do
+				Motor.Transform = defaultCF
+			end
+			if chr:FindFirstChild("Humanoid") then
+				if originalHipHeight ~= nil then
+					chr.Humanoid.HipHeight = originalHipHeight
 				end
-			end)
-		end
+				Instance.new("Animator", chr.Humanoid)
+			end
+			if chr:FindFirstChild("Animate") then
+				chr.Animate.Disabled = false
+			end
+		end)
 	end
 end
 
