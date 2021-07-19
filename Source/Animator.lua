e695f8b68aebb15ac9d85b9ea529486e45c00471
xhayper <46065349+xhayper@users.noreply.github.com>
2021-07-19 21:38:26 +0700

    *died*

--- Source/Animator.lua
+++ Source/Animator.lua
@@ -69,7 +69,7 @@ function Animator:Play(force)
 			Character.Animate.Disabled = true
 		end
 		local start = os.clock()
-		for i,f in ipairs(self.AnimationData.Frames) do
+		for i,f in pairs(self.AnimationData.Frames) do
 			print(f.Time * self.Speed and self.Speed >= 1 or f.Time / self.Speed)
 			if i ~= 1 and f.Time * self.Speed and self.Speed >= 1 or f.Time / self.Speed then
 				repeat print(f.Time, os.clock()-start); RunService.RenderStepped:Wait() until os.clock()-start > f.Time * self.Speed and self.Speed >= 1 or f.Time / self.Speed
