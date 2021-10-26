7d3f1df3e23456aab226d2104ee7f0deb442b9bb
xhayper <hayper1919@gmail.com>
2021-10-26 09:33:04 +0700

    Update AccessoryWeld-To-Motor6D.lua

--- Utility/AccessoryWeld-To-Motor6D.lua
+++ Utility/AccessoryWeld-To-Motor6D.lua
@@ -3,11 +3,12 @@ local AccessoryList = {}
 for _,i in next, path.to.Character:GetChildren() do
     if not i:IsA("Accessory") or 0 >= table.getn(AccessoryList) not table.find(AccessoryList, i.Name) then continue end
     i.Handle.Name = i.Name
-    local Motor = Instance.new("Motor6D", i[i.Name])
+    local Motor = Instance.new("Motor6D")
     Motor.Name = i.Name
     Motor.Part0 = i[i.Name].AccessoryWeld.Part1
     Motor.Part1 = i[i.Name].AccessoryWeld.Part0
     Motor.C0 = i[i.Name].AccessoryWeld.C0:Inverse()
     Motor.C1 = i[i.Name].AccessoryWeld.C1:Inverse()
+    Motor.Parent = i[i.Name]
     i[i.Name].AccessoryWeld:Destroy()
 end
