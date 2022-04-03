92ccb1279bb38a7f67c9b17daee21f5628e1b4a7
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-03 14:51:43 +0700

    feat: Ready to be pushed

--- Source/Main.lua
+++ Source/Main.lua
@@ -1,4 +1,4 @@
-local pathToGithub = "https://raw.githubusercontent.com/xhayper/Animator/rewrite/Source/"
+local pathToGithub = "https://raw.githubusercontent.com/xhayper/Animator/main/Source/"
 
 getgenv().httpRequireCache = getgenv().httpRequireCache or {}
 
